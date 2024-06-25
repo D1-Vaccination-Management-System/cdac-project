ca3985f2294001b571128a4ed69d53f599cd382c
vijayshiv <vijay.arya1213@gmail.com>
2024-06-25 19:35:25 +0530

    Layout and Routing done

--- vaccination-app/src/App.jsx
+++ vaccination-app/src/App.jsx
@@ -1,9 +1,23 @@
+import React from "react";
 import "./App.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import Layout from "./Layout";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [{ path: "/", element: <Home /> }],
+  },
+]);
 function App() {
   return (
     <>
-      <div>Project start</div>
+      <RouterProvider router={router} />
+      <ToastContainer />
     </>
   );
 }
---
+++ vaccination-app/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home() {
+  return (
+    <>
+      <div>
+        <h1>Hee</h1>
+      </div>
+    </>
+  );
+}
+
+export default Home;
