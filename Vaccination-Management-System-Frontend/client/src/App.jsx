88b0e6fb8f745bccae7549ed315715cd7faa203a
vijayshiv <vijay.arya1213@gmail.com>
2024-08-13 13:49:28 +0530

    frontent started

---
+++ Vaccination-Management-System-Frontend/client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="capitalize bg-pink-500 rounded-sm">Hello VMS group</h1>
+    </>
+  );
+}
+
+export default App;
